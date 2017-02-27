7d1b9a35af6e8d758d3bfa157ae3f4946ba9dc4b
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2017-02-27 23:31:20 +0100

    :tada:

---
+++ injectDependencies.js
@@ -0,0 +1,27 @@
+"use strict";
+const fs = require("fs");
+require("shelljs/global");
+
+const server = "";
+const scripts = [];
+const stylesheets = [];
+const folders = ["."];
+const find = "";
+
+function css(url) {
+  return '<link rel="stylesheet" href="' + url + '.css"/>\n'
+}
+function js(url) {
+  return '<script src="' + url + '.js"></script>\n';
+}
+
+const injection = "";
+stylesheets.forEach(stylesheet => injection += css(server + stylesheet));
+        scripts.forEach(script => injection +=  js(server + script));
+
+folders 
+fs.readdir(".", (err, files) => {
+  files.filter(file => file.endsWith(".html")).forEach(file => {
+    sed("-i", new RegExp(find), find + "\n" + injection, file);
+  });
+});
