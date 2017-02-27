4b41b0c9fddd074588c235de03d87f0b198f1651
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2017-02-28 00:01:52 +0100

    defer inline scripts after pageload

--- injectDependencies.js
+++ injectDependencies.js
@@ -15,6 +15,19 @@ function js(url) {
   return '<script src="' + url + '.js"></script>\n';
 }
 
+// http://stackoverflow.com/a/29680837/3857476
+let loadInlineScripts = `<script>
+  window.addEventListener("load", () => {
+    var arr = document.querySelectorAll("script[type='later']");
+    for (var x = 0; x < arr.length; x++) {
+      var cln = arr[x].cloneNode(true);
+      cln.type = "text/javascript";
+      document.querySelectorAll("body")[0].appendChild(cln);
+    }
+    console.log(new Date() + "| deferred script load");
+  });
+</script>`;
+
 let injection = "";
 stylesheets.forEach(stylesheet => injection += css(server + stylesheet));
         scripts.forEach(script => injection +=  js(server + script));
@@ -23,5 +36,9 @@ folders
 fs.readdir(".", (err, files) => {
   files.filter(file => file.endsWith(".html")).forEach(file => {
     sed("-i", new RegExp(find), find + "\n" + injection, file);
+
+    // Defer inline scripts
+    sed("-i", /<script>[\r\n]/, "<script type='later'>\n", file);
+    fs.appendFile(file, loadInlineScripts, "utf8");
   });
 });
